d1c0aeb43dc0a90ab5b1ba2ce89f183e5782dbef
anjali-bindal <ashish@antino.io>
2022-05-30 15:43:32 +0530

    started

---
+++ src/login/login.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./login.css";
+
+export default function login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
+
+
+}
